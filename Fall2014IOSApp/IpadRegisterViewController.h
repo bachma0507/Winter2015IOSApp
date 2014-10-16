61f6ec1f4268aac88b60c4c37e08da95bde74ffe
Barry <bachma0507@gmail.com>
2014-10-16 15:40:41 -0400

    First Commit
    First Commit

---
+++ Fall2014IOSApp/IpadRegisterViewController.h
@@ -0,0 +1,20 @@
+//
+//  IpadRegisterViewController.h
+//  Winter2014IOSApp
+//
+//  Created by Barry on 10/14/13.
+//  Copyright (c) 2013 BICSI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IpadRegisterViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UITextField *userRegisterTextField;
+@property (nonatomic, strong) IBOutlet UITextField *passwordRegisterTextField;
+
+
+-(IBAction)signUpUserPressed:(id)sender;
+
+@end
+
---
+++ Fall2014IOSApp/IpadRegisterViewController.m
@@ -0,0 +1,92 @@
+//
+//  IpadRegisterViewController.m
+//  Winter2014IOSApp
+//
+//  Created by Barry on 10/14/13.
+//  Copyright (c) 2013 BICSI. All rights reserved.
+//
+
+#import "IpadRegisterViewController.h"
+
+#import <Parse/Parse.h>
+
+@interface IpadRegisterViewController ()
+
+@end
+
+@implementation IpadRegisterViewController
+
+@synthesize userRegisterTextField = _userRegisterTextField, passwordRegisterTextField = _passwordRegisterTextField;
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    [TestFlight passCheckpoint:@"GalleryIpadRegPage-info-viewed"];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    self.userRegisterTextField = nil;
+    self.passwordRegisterTextField = nil;
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    NSLog(@"touchesBegan:withEvent:");
+    [self.view endEditing:YES];
+    [super touchesBegan:touches withEvent:event];
+}
+
+
+
+#pragma mark IB Actions
+
+////Sign Up Button pressed
+-(IBAction)signUpUserPressed:(id)sender
+{
+   
+    [TestFlight passCheckpoint:@"GalleryIpadRegButton-pressed"];
+    
+    PFUser *user = [PFUser user];
+    user.username = self.userRegisterTextField.text;
+    user.password = self.passwordRegisterTextField.text;
+    
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            //The registration was succesful, go to the wall
+            [self performSegueWithIdentifier:@"SignupSuccesful" sender:self];
+            
+        } else {
+            //Something bad has ocurred
+            NSString *errorString = [[error userInfo] objectForKey:@"error"];
+            UIAlertView *errorAlertView = [[UIAlertView alloc] initWithTitle:@"Error" message:errorString delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+            [errorAlertView show];
+        }
+    }];
+    
+    
+}
+
+@end
+
